1098b1cbcf0e6d5da92d14d5a0139bf065d60c05
Estelo M Abellanosa <49965889+estelo2jz@users.noreply.github.com>
2020-12-08 13:41:26 -0600

    added checkout menu

--- src/components/Reducer.js
+++ src/components/Reducer.js
@@ -23,9 +23,16 @@ export const initialState = {
     },
   ],
   user: null,
-}
+};
 
-function reducer(state, action) {
+// import { useStateValue } from './StateProvider';
+// const [{basket}] = useStateValue();
+
+export const getBasketTotal = (basket) => 
+basket?.reduce((amount, item) => item.price + amount, 0)
+
+
+const reducer = (state, action) => {
   console.log(action);
   switch(action.type) {
     case 'ADD_TO_BASKET':
@@ -37,8 +44,11 @@ function reducer(state, action) {
       break;
     case 'REMOVE_FROM_BASKET':
       // Logic removing item from basket
+
+      // we cloned the basket
       let newBasket = [...state.basket];
 
+      // we check to see if product exists
       const index = state.basket.findIndex((basketItem) => basketItem.id === action.id)
 
       if (index >= 0) {
@@ -48,11 +58,10 @@ function reducer(state, action) {
         // console.warn is a red console.log
         console.warn(
           `Cant remove product (id: ${action.id} as its no)`
-        )
+        );
       }
 
       return { ...state, basket: newBasket };
-      break;
     default:
       return state;
   }
